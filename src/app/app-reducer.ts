90b0debdbc7e85beb2fa0dd334ac00b61e7e414f
lergnom <lergnom@mail.ru>
2021-10-26 10:11:22 +0300

    add initialised extrareducer

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -1,17 +1,30 @@
-import {Dispatch} from 'redux';
 import {authAPI} from '../api/todolists-api';
 import {setIsLoggedInAC} from "../features/Login/auth-reducer";
-import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
 
-const initialState: InitialStateType = {
-    status: 'idle',
-    error: null,
-    isInitialized: false
-};
+export const initializeAppTC = createAsyncThunk('app/initialize', async (arg, {dispatch}) => {
+    try {
+        const res = await authAPI.me();
+        if (res.data.resultCode === 0) {
+            dispatch(setIsLoggedInAC({value: true}));
+            return;
+        } else {
+        }
+        return;
+    } catch (e) {
+
+    } finally {
+    }
+});
+
 
 const slice = createSlice({
     name: "app",
-    initialState: initialState,
+    initialState: {
+        status: 'idle',
+        error: null,
+        isInitialized: false
+    } as InitialStateType,
     reducers: {
         setAppErrorAC(state, action: PayloadAction<{ error: string | null }>) {
             state.error = action.payload.error;
@@ -19,9 +32,11 @@ const slice = createSlice({
         setAppStatusAC(state, action: PayloadAction<{ status: RequestStatusType }>) {
             state.status = action.payload.status;
         },
-        setAppInitializedAC(state, action: PayloadAction<{ value: boolean }>) {
-            state.isInitialized = action.payload.value;
-        }
+    },
+    extraReducers: builder => {
+        builder.addCase(initializeAppTC.fulfilled, (state) => {
+            state.isInitialized = true;
+        });
     }
 });
 
@@ -36,16 +51,6 @@ export type InitialStateType = {
     // true когда приложение проинициализировалось (проверили юзера, настройки получили и т.д.)
     isInitialized: boolean
 }
-export const {setAppInitializedAC, setAppErrorAC, setAppStatusAC} = slice.actions;
+export const {setAppErrorAC, setAppStatusAC} = slice.actions;
 
-
-export const initializeAppTC = () => (dispatch: Dispatch) => {
-    authAPI.me().then(res => {
-        if (res.data.resultCode === 0) {
-            dispatch(setIsLoggedInAC({value: true}));
-        } else {
-        }
-        dispatch(setAppInitializedAC({value: true}));
-    });
-};
 
